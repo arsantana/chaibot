e8f1489bc0bcda9f90fe97791b35ecbfa1e5683f
Arleen Santana <santana.arleen@gmail.com>
2020-11-24 01:33:34 -0500

    sidebar and navigation bar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as AiIcons from "react-icons/ai"
+
+
+export const Sidebar = [
+  {
+    title: 'home',
+    path: '/',
+    icon:<AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'chaibot',
+    path: '/404',
+    icon:<AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'feed',
+    path: '/page-2',
+    icon:<AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'help',
+    path: '/',
+    icon:<AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  }
+]
